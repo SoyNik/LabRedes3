a8a1d7a2ad0582f73c5946804ab704d3cb6bc5d5
Nicolás <n.segura@uniandes.edu.co>
2021-10-04 18:15:03 -0500

    Commit final

---
+++ ServidorPruebas.py
@@ -0,0 +1,171 @@
+import socket
+import hashlib
+import threading
+import time
+import datetime
+import os
+
+lock= threading.Lock()
+
+def pedirDatos():
+    fileName = ""
+    fileT = ""
+    entr = int(input("Ingrese archivo que quiere enviar 1 (100 MB) o 2 (250MB)"))
+    if (entr == 1):
+        fileName = "../Doc/Prueba4.mp4"
+        fileT = ".mp4"
+    elif (entr == 2):
+        fileName = "../Doc/Prueba5.mp4"
+        fileT = ".mp4"
+    elif (entr == 3):
+        print("Prueba con archivo de 11MB")
+        fileName = "../Doc/Prueba3.pdf"
+        fileT = ".pdf"
+    entr = int(input("Ingrese el numero de clientes en simultaneo a enviar el archivo"))
+    numClientes = entr
+    return fileName, fileT, numClientes
+
+
+tup = pedirDatos()
+fileName = tup[0]
+numClientes = tup[2]
+fileT = tup[1]
+numClientesC=0
+atender=False
+
+def servidor():
+    global numClientesC
+    global atender
+
+    print("Server listening....")
+
+    while True:
+        conn, addr = s.accept()  # Establish connection with client.
+
+        numClientesC += 1
+        print("Numero Clientes Conectados: ", numClientesC)
+        # print('Got connection from', addr)
+        data = conn.recv(BUFF)
+        print('Server received', data.decode())
+        if(data.decode()=="READY"):
+            sha1 = hashlib.sha1()
+            while True:
+                if (numClientesC >= numClientes or atender):
+                    print("Starting to send")
+                    break
+            atender=True
+            i = 0
+            conn.send(fileT.encode())
+            # print("Tipo del archivo a enviar: ",fileT.encode())
+
+            time.sleep(0.01)
+
+            inicioT = time.time()
+            with open(fileName, 'rb') as f:
+                # print("Starting to send")
+                while True:
+                    i += 1
+                    data = f.read(BUFF)
+
+                    if not data:
+                        break
+                    sha1.update(data)
+
+                    conn.send(data)
+
+                print("Archivo Enviado")
+                # Envio de Hash
+                has = str(sha1.hexdigest())
+                conn.send(("FINM" + has).encode())
+
+                f.close()
+
+                data = conn.recv(BUFF)
+                # Notificacion de recepcion
+                datosCiente = data.decode().split("/")
+                recepcion = datosCiente[1]
+                print(recepcion)
+
+                # Notificacion de tiempo
+                finT = float(datosCiente[2])
+                totalT = finT - inicioT
+                # print("Tiempo total:",totalT, "Segundos")
+
+                # Numero de paquetes recibidos por el cliente
+                paqRecv = datosCiente[0]
+
+                hashR=datosCiente[4]
+                logDatosCliente(recepcion, totalT, i, paqRecv,hashR,has)
+
+                print('Fin envio')
+                numClientesC -= 1
+                print("Numero Clientes Conectados: ", numClientesC)
+
+                # Notificacion de fin de cliente o no
+                terminS = datosCiente[3]
+                # print("Mensaje del cliente: ", terminS)
+
+                conn.close()
+                if (terminS == "TERMINATE"):
+                    print(terminS)
+                    break
+    print("Fin Servidor")
+
+
+def createLog():
+    print("Creando log")
+
+    # Fecha y hora --creacion log
+    fecha = datetime.datetime.now()
+
+    logName = "Logs/log" + str(fecha.timestamp()) + ".txt"
+    logFile = open(logName, "a")
+    logFile.write("Fecha: " + str(fecha) + "\n")
+
+    # Nombre del archivo y tamanio
+    fileN = fileName.split("/")
+    fileN = fileN[1]
+
+    logFile.write("Nombre del archivo: " + fileN + "\n")
+
+    fSize = os.path.getsize(fileName)
+
+    logFile.write("Tamanio del archivo: " + str(fSize) + " bytes\n")
+    logFile.write("----------------------------------------\n")
+
+    logFile.close()
+    return logName
+
+
+# Crear el archivo de log
+logName = createLog()
+
+
+def logDatosCliente(recepcion, tiempo, numPaqEnv, numPaqRecv,hashR, hash):
+
+    with lock:
+        paquetesE="Numero de paquetes enviados por el servidor:" + str(numPaqEnv) + "\n"
+        paquetesR="Numero de paquetes recibidos por el cliente:" + str(numPaqRecv) + "\n"
+        tiempoT="Tiempo: " + str(tiempo) + " segundos\n"
+        separador="\n---------------------------------------\n"
+        hash="\nHASH calculado en el servidor: \n"+hash
+        logFile = open(logName, "a")
+        logFile.write(recepcion+"\n"+paquetesE+paquetesR+tiempoT+hashR+hash+separador )
+        logFile.close()
+
+# Puerto, buffer
+port = 9999
+BUFF = 2048
+
+# TCP ------> socket.AF_INET, socket.SOCK_STREAM
+# UDP ------> socket.AF_INET, socket.SOCK_DGRAM
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = ""
+s.bind((host, port))
+
+# Por parametro el numero de conexiones que se pueden aceptar antes de rechazar nuevas conexiones
+s.listen(25)
+
+for i in range(25):
+    t= threading.Thread(target=servidor, args=())
+    t.start()
---
+++ TCPClientPruebas.py
@@ -0,0 +1,150 @@
+import datetime
+import os
+import socket
+import threading
+import hashlib
+import time
+
+BUFF=2048
+lock= threading.Lock()
+
+def cliente(num, last,lock):
+    datosLog=""
+    mensajesConsola=[]
+    mensajesConsola.append("Cliente #"+str(num))
+
+    # TCP ------> socket.AF_INET, socket.SOCK_STREAM
+    # UDP ------> socket.AF_INET, socket.SOCK_DGRAM
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # host = "34.228.233.12"
+    host = "localhost"
+    port = 9999
+
+    i=0
+
+    s.connect((host, port))
+    mensajesConsola.append("Conexion con el servidor")
+    s.send("READY".encode())
+    mensajesConsola.append("Listo para recibir")
+
+    fTipo=""
+    while True:
+        data = s.recv(BUFF)
+        if(data.__contains__(b".")):
+            fTipo = data
+            break
+    # print("TIPO:",fTipo)
+    finT=0
+
+    hashR=""
+    sha1=hashlib.sha1()
+    fileName="Recibido/received_file"+str(num)+fTipo.decode()
+    with open(fileName, 'wb') as f:
+        mensajesConsola.append("Recibiendo archivo")
+        with lock:
+            while True:
+                # print('receiving data...',i)
+                i+=1
+                data = s.recv(BUFF)
+
+                if not data:
+                    break
+
+                elif (data.__contains__(b"FINM")):
+                    val=data.find(b"FINM")
+                    sha1.update(data[:val])
+                    hashR = data[val:]
+                    finT=time.time()
+
+                    break
+                else:
+                    sha1.update(data)
+                    f.write(data)
+    f.close()
+    mensajesConsola.append("Archivo recibido")
+
+    # Numero de paquetes recibidos
+    datosLog+=str(i)+"/"
+
+    hashR=hashR[4:].decode()
+    mensajesConsola.append("Cliente"+str(num)+"Hash Recibido: \n"+str(hashR))
+    mensajesConsola.append("Cliente"+str(num)+"Hash Calculado:\n"+str(sha1.hexdigest()))
+
+    notif=""
+    if(hashR==sha1.hexdigest()):
+        notif="Exito"
+        mensajesConsola.append("Archivo recibido Exitosamente")
+    else:
+        notif = "Error"
+        mensajesConsola.append("El Hash del archivo recibido es diferente del calculado")
+
+    # Notificacion de recepcion
+    mensajesConsola.append("Envio de notificacion")
+    recepcion="Cliente " + str(num) + " termino con estado de " + notif
+    datosLog+=recepcion+"/"
+
+    # Envio de tiempo
+    datosLog+=str(finT)+"/"
+
+    # Si es el ultimo, mandar fin para el servidor tambien
+    if(last):
+        datosLog+="TERMINATE/"
+    else:
+        datosLog+="CONTINUE/"
+
+    datosLog+="HASH calculado en el cliente\n"+str(sha1.hexdigest())
+
+    # print(datosLog)
+    s.send(datosLog.encode())
+    logDatosCliente(recepcion,i,hashR,sha1.hexdigest(),fileName)
+
+    for i in mensajesConsola:
+        print(i)
+
+    s.close()
+
+def createLog():
+    print("Creando log")
+
+    # Fecha y hora --creacion log
+    fecha = datetime.datetime.now()
+
+    logName = "LogsCliente/logC" + str(fecha.timestamp()) + ".txt"
+    logFile = open(logName, "a")
+    logFile.write("Fecha: " + str(fecha) + "\n")
+
+    logFile.write("----------------------------------------\n")
+
+    logFile.close()
+    return logName
+
+cantidadCliente=25
+lock=threading.Lock()
+file=createLog()
+
+def logDatosCliente(recepcion, numPaqRecv,hashR, hash,fileName):
+
+    with lock:
+        # # Nombre del archivo y tamanio
+        fileN=fileName.split("/")
+        fileN = "Nombre del archivo "+fileN[1]+"\n"
+        fSize = os.path.getsize(fileName)
+        size="Tamanio del archivo: " + str(fSize) + " bytes\n"
+
+        paquetesR="Numero de paquetes recibidos por el cliente:" + str(numPaqRecv) + "\n"
+        separador="\n---------------------------------------\n"
+        hash="\nHASH calculado en el cliente: \n"+hash
+        hashR="\nHASH calculado en el servidor: \n"+hashR
+        logFile = open(file, "a")
+        logFile.write(fileN+size+recepcion+"\n"+paquetesR+hashR+hash+separador )
+        logFile.close()
+
+for i in range(cantidadCliente):
+    if(i==cantidadCliente-1):
+        t = threading.Thread(target=cliente,args=(i, True,lock))
+        t.start()
+    else:
+        t = threading.Thread(target=cliente,args=(i, False,lock))
+        t.start()
+
+# print("FIN")
